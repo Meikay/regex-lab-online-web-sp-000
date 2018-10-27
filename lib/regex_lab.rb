0dcca713630821cc26a43dcec7fcbc5ac971df8b
Meisuleen Kased <meisuleen@gmail.com>
2018-10-27 01:38:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,11 @@
+require 'pry'
+binding.pry
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiou]i/)
+    return true
+  else
+    return nil
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
